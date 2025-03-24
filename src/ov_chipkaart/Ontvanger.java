172c35e39364e5204ba690b933720faa70b53fb5
KamielFlamiel <justinmeijer2007@outlook.com>
2025-03-24 11:38:50 +0100

    wah

--- src/ov_chipkaart/Ontvanger.java
+++ src/ov_chipkaart/Ontvanger.java
@@ -1,12 +1,14 @@
 package ov_chipkaart;
 
 public class Ontvanger {
-    private static final double INSTAP_TARIEF = 4.00; // 
+    private static final double Instap_Tarief = 4.00; 
+    private static final double ReisPrijs = 3.00;
+    
 
     public boolean incheck(Klant klant) {
-        if (klant.getSaldo() >= INSTAP_TARIEF) {
+        if (klant.getSaldo() >= Instap_Tarief) {
             
-            klant.setSaldo(klant.getSaldo() - INSTAP_TARIEF);
+            klant.setSaldo(klant.getSaldo() - Instap_Tarief);
             System.out.println("Inchecken geslaagd! Instaptarief is afgeschreven.");
             return true;
         } else {
@@ -14,6 +16,15 @@ public class Ontvanger {
             System.out.println("Saldo is onvoldoende om in te checken.");
             return false;
         }
+    }
+    
+    public boolean uitchecken(Klant klant) {
+        if (klant.getSaldo() >= ReisPrijs) {
+            klant.setSaldo(klant.getSaldo() - ReisPrijs);
+            System.out.println("Uitchecken geslaagd! Reisprijs is afgeschreven.");
+            return true;
+        }
+        return false;
     }
 
 }
