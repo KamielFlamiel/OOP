4de8e7444bdde825e63370bdee50ca6b25a575b5
KamielFlamiel <justinmeijer2007@outlook.com>
2025-04-07 11:59:10 +0200

    Saldo opladen en loop toegevoegd

--- src/ov_chipkaart/OV_Chipkaart.java
+++ src/ov_chipkaart/OV_Chipkaart.java
@@ -1,19 +1,46 @@
 package ov_chipkaart;
 
+import java.util.Scanner;
+
 public class OV_Chipkaart {
     public static void main(String[] args) {
-        
+    	
+    	Scanner sc = new Scanner(System.in);
         Saldo_Systeem klant = new Saldo_Systeem(20.00);
-
         Poortje ovSaldo = new Poortje();
-
-        ovSaldo.incheck(klant);
-
-        System.out.println("Huidig saldo na inchecken: " + klant.getSaldo());
         
-        ovSaldo.uitcheck(klant);
+        while(true) {
+        	
+        	System.out.println("Je saldo = " + klant.getSaldo());
+        	System.out.println("Wil je inchecken/uitchecken of OV-kaart opladen?");
+            System.out.println("1 voor inchecken/uitchecken, 2 voor OV-kaart opladen");
+            
+            int input = sc.nextInt();
+            
+            if (input != (1 & 2)) {
+            	System.out.println("Verkeerde input");
+            }
+
+            if (input == 1) {
+            	ovSaldo.incheck(klant);
+
+                System.out.println("Huidig saldo na inchecken: " + klant.getSaldo());
+                
+                ovSaldo.uitcheck(klant);
+                
+                System.out.println("Huidig saldo na uitchecken: " + klant.getSaldo());
+            } else if (input == 2) {
+            	System.out.println("Hoeveel wil je opladen?");
+            	int saldo = sc.nextInt();
+            	klant.setSaldo(saldo);
+            	
+            	System.out.println("Nieuw saldo: €" + klant.getSaldo());
+            }
+            
+        }
         
-        System.out.println("Huidig saldo na uitchecken: " + klant.getSaldo());
+        
+        
         
         
     }
